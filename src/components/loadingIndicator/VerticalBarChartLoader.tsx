d41092438d5439e2c51309e9ebc9fa6035ff1aba
Hichem Ben <hichemben@MBP-van-Hichem.home>
2024-08-30 21:35:40 +0200

    refactor: improve bar char loader

--- src/components/loadingIndicator/VerticalBarChartLoader.tsx
+++ src/components/loadingIndicator/VerticalBarChartLoader.tsx
@@ -4,20 +4,24 @@ const VerticalBarChartLoader = () => {
     { height: "50%" },
     { height: "90%" },
     { height: "60%" },
+    { height: "40%" },
     { height: "30%" },
+    { height: "75%" },
+    { height: "65%" },
     { height: "80%" },
     { height: "40%" },
     { height: "75%" },
+    { height: "55%" },
     { height: "55%" },
     { height: "65%" },
   ];
 
   return (
-    <div className="flex items-end justify-between space-x-1 h-48 w-full">
+    <div className="flex items-end justify-between space-x-1 h-72 w-full pt-12 px-6">
       {bars.map((bar, index) => (
         <div
           key={index}
-          className="w-4 bg-gray-300 rounded animate-pulse"
+          className="w-8 bg-gray-300 rounded animate-pulse"
           style={{ height: bar.height }}
         ></div>
       ))}
